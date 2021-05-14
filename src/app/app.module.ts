bbc5c4550bc2b1ae56fc5933cb2852b696d6de7d
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-05-14 09:20:40 -0700

    1. Bootstrap Angular App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUserComponent,
+    ListUserComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
